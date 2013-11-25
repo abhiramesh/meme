74f0b8ed49a64af6d2e59c39a8dd432e79d61b88
Abhi Ramesh <abhiramesh@gmail.com>
2013-11-25 17:32:05 -0500

    fix app cont

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for(resource)
-  	create_meme_path
+  	'/new_meme'
   end
 
   def after_sign_out_path_for(resource)
