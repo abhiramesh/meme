56e6352031a7d44086d7efeb80f2988178436349
Abhi Ramesh <abhiramesh@gmail.com>
2013-11-27 21:29:49 -0500

    fix user photos=

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,7 @@ class Photo < ActiveRecord::Base
   attr_accessible :uid, :src, :viewer_id, :user_id
 
   belongs_to :friend, :foreign_key => :uid, :primary_key => :uid
-  belongs_to :user
+  belongs_to :user, :foreign_key => :user_id, :primary_key => :user_id
   has_many :memes, dependent: :destroy
 
 end
